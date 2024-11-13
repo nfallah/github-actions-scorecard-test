689f5660f1e0dbe26e68fd82bc72693c6f256aeb
Nima Fallah <nfallah399@gmail.com>
2024-11-13 17:34:13 -0500

    Added markdown table feature

--- generate_scorecard.py
+++ generate_scorecard.py
@@ -1,20 +1,36 @@
 import subprocess
 import json
+from scorecard_template import MARKDOWN_TEMPLATE
 
 def main():
-    i = 0
     with open('config.txt', 'r') as file:
         urls = file.read().splitlines()
-        #print(urls)
     for url in urls:
         args = ['scorecard', '--repo', url, '--format', 'json']
         result = subprocess.run(args, capture_output=True, text=True)
-        with open(f'report_{i}.json', 'w') as file2:
-            str = result.stdout
-            jsonfile = json.loads(result.stdout)
-            file2.write(str)
-            #TODO: work with jsonfile to generate MD table
-        i += 1
+        create_markdown(json.loads(result))
+
+def create_markdown(jsonfile):
+    checks = []
+    for check in jsonfile['checks']:
+        score = check['score']
+        if score < 0: score = '*N/A*'
+        name = f'[{check['name']}]({check['documentation']['url']})'
+        assessment = '*N/A*' if score == '*N/A*' else (1 if score <= 3.3 else 2 if score <= 6.6 else 3 if score <= 9.9 else 10)
+        notes = check['reason']
+        checks.append(f'|{score}|{name}|{assessment}|{notes}|')
+    checks_md = '\n'.join(checks)
+    markdown_data = MARKDOWN_TEMPLATE.format(
+        scorecard_date = jsonfile['date'],
+        scorecard_version = jsonfile['scorecard']['version'],
+        scorecard_score = jsonfile['score'],
+        scorecard_rows = checks_md
+    )
+    elements = check['repo']['name'].split('/')
+    name = elements[2]
+    owner = elements[1]
+    with open(f'{owner}_{name}.md', 'w') as file:
+        file.write(markdown_data)
 
 if __name__ == "__main__":
     main()
---
+++ scorecard_template.py
@@ -0,0 +1,11 @@
+MARKDOWN_TEMPLATE = """\
+#### **Scorecard**
+
+|Date            |Version            |Score            |
+|----------------|-------------------|-----------------|
+|{scorecard_date}|{scorecard_version}|{scorecard_score}|
+
+|Score|Name|Assessment|Notes|
+|-----|----|----------|-----|
+{scorecard_rows}
+"""
