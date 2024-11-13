80786f2e2931219beb170c0b2d35386a58f0ea8b
Nima Fallah <nfallah399@gmail.com>
2024-11-13 13:46:17 -0500

    updated config and script for testing

--- generate_scorecard.py
+++ generate_scorecard.py
@@ -4,12 +4,16 @@ import json
 def main():
     i = 0
     with open('config.txt', 'r') as file:
-        urls = file.readlines()
+        urls = file.read().splitlines()
+        #print(urls)
     for url in urls:
         args = ['scorecard', '--repo', url, '--format', 'json']
         result = subprocess.run(args, capture_output=True, text=True)
-        with open(f'{i}.json', 'w') as file2:
-            file2.write(json.loads(result.stdout))
+        with open(f'report_{i}.json', 'w') as file2:
+            str = result.stdout
+            jsonfile = json.loads(result.stdout)
+            file2.write(str)
+            #TODO: work with jsonfile to generate MD table
         i += 1
 
 if __name__ == "__main__":
